08a74925a3c7c75523d9b74fea1b52d53585f2b1
Jorge <j.org.ls@ciencias.unam.mx>
2018-10-04 04:05:17 +0200

    beta version

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import sys
-import design
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtCore import QCoreApplication
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QAction, QMessageBox
-from PyQt5.uic.properties import QtGui
+import os
+import cv2
+import scripts.design as design
+from image_restorer import*
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtWidgets import QMenu, QMainWindow, QPushButton, QAction, QMessageBox
+from PyQt5.QtWidgets import QFileDialog, QApplication, QVBoxLayout, QSizePolicy, QWidget
 
 class ExampleApp(QMainWindow, design.Ui_MainWindow):
     def __init__(self):
         super(ExampleApp, self).__init__()
         self.setupUi(self)
+        self.file_name = 'images/white.png'
+        self.save_name = ''
+        self.image_restored = 0
+        self.restoration_method = ''
         
-	
-        # Actions
+        # ----Actions---
+        
         self.actionQuit.setShortcut('Ctrl+Q')
         self.actionQuit.setStatusTip('Leave the app')
         self.actionQuit.triggered.connect(self.close_application)
@@ -25,59 +30,142 @@ class ExampleApp(QMainWindow, design.Ui_MainWindow):
         self.actionSave_Image.setShortcut('Ctrl+S')
         self.actionSave_Image.setStatusTip('Save image')
         self.actionSave_Image.triggered.connect(self.save_image)
-
         self.actionRestore_Image.setShortcut('Shift+Ctrl+R')
         self.actionRestore_Image.setStatusTip('Save image')
         self.actionRestore_Image.triggered.connect(self.restore_image)
         self.actionSelect_specific_area.setShortcut('Shift+Ctrl+A')
-        self.actionSelect_specific_area.setStatusTip('Save image')
+        self.actionSelect_specific_area.setStatusTip('Select are')
         self.actionSelect_specific_area.triggered.connect(self.select_region)
 
-        
-        self.actionQuit.triggered.connect(self.close_application)
+        #self.rubberBand = QRubberBand(QRubberBand.Rectangle, self)
 
-        scene = QtWidgets.QGraphicsScene()
-        self.graphicsView.setScene(scene)
-        #pen = QtGui.QPen(QtCore.Qt.green)
+        # ---Buttons---
+       
+        self.toolButton.clicked.connect(self.load_image)
+        self.spinBox.setValue(1)
+        self.spinBox.setMinimum(1)
+        self.comboBox.activated.connect(self.handleActivated)
+        self.comboBox.addItems(['Select a Method', 'Diffusion', 'Diffusion_f', 'Diffusion_c'])
+        self.pushButton.clicked.connect(self.restore_image)
+        self.pushButton_2.clicked.connect(self.save_image)
 
-        #side = 20
-
-        #for i in range(16):
-        #    for j in range(16):
-        #        r = QtCore.QRectF(QtCore.QPointF(i*side, j*side), QtCore.QSizeF(side, side))
-        #        scene.addRect(r, pen)
-
-
+        pixmap = QPixmap(self.file_name)
+        self.label_6.setPixmap(pixmap) 
+        self.label_7.setPixmap(pixmap)
+        self.resize(pixmap.width(),pixmap.height())
+        self.show()
 
     def close_application(self):
-
         choice = QMessageBox.question(self, 'Close!',
                                      "Are you sure you want  to quit?", QMessageBox.Yes |
-                                     QMessageBox.No, QMessageBox.No)
+                                     QMessageBox.No)
         if choice == QMessageBox.Yes:
             sys.exit()
+        else: 
+            pass
+        
+        self.show()
+        
+    def new_simulation(self):
+        self.spinBox.setValue(1)
+        self.label_5.setText('0.0')
+        self.progressBar.setProperty("value", 0)
+        pixmap = QPixmap('white.png')
+        self.label_6.setPixmap(pixmap) 
+        self.label_7.setPixmap(pixmap) 
+        self.resize(pixmap.width(),pixmap.height())
+        self.show()      
+    
+    def handleActivated(self, index):
+        pass
+
+    def save_image(self):
+        save_name, _ = QFileDialog.getSaveFileName(self, 'Save Image')
+        cv2.imwrite(save_name, self.image_restored)
+
+    def load_image(self):
+        ext_options = ['.png', '.jpg', '.bmp']
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        name, _ = QFileDialog.getOpenFileName(self,"Open", "",
+                                                "All Files (*);;Python Files (*.py);;PNG (*.png);;JPG (*.jpg);;BMP (*.bmp)", 
+                                                options=options)
+        self.file_name = name
+        if name:
+            if name[-4:] not in ext_options:
+                im_choice = QMessageBox.question(self, 'Error!',
+                                         "The file selected is not an image or the format is not supported", 
+                                         QMessageBox.Ok)    
+            else:
+                pixmap = QPixmap(str(name))
+                self.label_6.setPixmap(pixmap)
+                pixmap1 = QPixmap('images/white.png')
+                self.label_7.setPixmap(pixmap1)
+                self.resize(pixmap.width(),pixmap.height())
+                self.show()
         else:
             pass
-    def new_simulation(self):
-        pass
-    
-    def save_image(self):
-        pass
-        
-    def load_image(self):
-        
-        self.setScene(QGraphicsScene())
-        self.path = QPainterPath()
-        self.item = GraphicsPathItem()
-        self.scene().addItem(self.item)
-        pass
-    
+        #self.show()
+
     def restore_image(self):
-        pass
+        new_ima = QPixmap('temp/white.png')
+        self.label_7.setPixmap(new_ima)
+
+        # --- if there is an image to compare with
+        if self.checkBox.isChecked() is True:
+            QMessageBox.question(self, 'Atention!',
+                                 "Select the original image to compare with",
+                                 QMessageBox.Ok)
+
+            ext_options = ['.png', '.jpg', '.bmp']
+            options = QFileDialog.Options()
+            options |= QFileDialog.DontUseNativeDialog
+            im_comparison, _ = QFileDialog.getOpenFileName(self, "Open", "",
+                                                           "All Files (*);;Python Files (*.py);;PNG (*.png);;JPG (*.jpg);;BMP (*.bmp)",
+                                                           options=options)
+
+            if im_comparison:
+                if im_comparison[-4:] not in ext_options:
+                    QMessageBox.question(self, 'Error!',
+                                         "The file selected is not an image or the format is not supported",
+                                         QMessageBox.Ok)
+        else:
+            im_comparison = self.file_name
+
+        if self.comboBox.currentText() != 'Select a Method':
+            # --- do the restoration---
+            for i in range(self.spinBox.value()):
+                ima, xi = image_restorer(self.file_name, self.comboBox.currentText(), im_comparison, plots=False)
+
+            self.image_restored = ima
+            self.label_5.setText(str(xi))
+            cv2.imwrite("".join([os.getcwd(), '/temp/', 'temp.png']), ima)
+            new_ima = QPixmap('temp/temp.png')
+            self.label_7.setPixmap(new_ima)
+
+        else:
+            QMessageBox.question(self, 'Error!',
+                                         "Select a restoration method", 
+                                         QMessageBox.Ok)  
     
-    def select_region():
-        pass
-        
+    def select_region(self):
+
+        def mousePressEvent(self, event):
+
+            if event.button() == Qt.LeftButton:
+                self.origin = QPoint(event.pos())
+                self.rubberBand.setGeometry(QRect(self.origin, QSize()))
+                self.rubberBand.show()
+
+        def mouseMoveEvent(self, event):
+
+            if not self.origin.isNull():
+                self.rubberBand.setGeometry(QRect(self.origin, event.pos()).normalized())
+
+        def mouseReleaseEvent(self, event):
+
+            if event.button() == Qt.LeftButton:
+                self.rubberBand.hide()
 
 
 def main():
---
+++ scripts/restr_methods/Diffusion.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+@author: jl
+"""
+import numpy as np
+import copy
+
+
+# --- diffusion with cyclical boundary conditions---
+def Diffusion_c(im, D, dx):
+    
+    im_var = im.copy()
+    for i in range(len(im)):
+        for j in range(len(im.T)):
+            
+            if i == 0:
+                u1 = im[len(im) - 1, j]
+            else:
+                u1 = im[i - 1, j]
+            
+            if i == len(im) - 1:
+                u2 = im[0, j]
+            else:
+                u2 = im[i + 1, j]
+            
+            if j == 0:
+                u3 = im[i, len(im.T) - 1]
+            else:
+                u3 = im[i, j - 1]
+            
+            if j == len(im.T) - 1:
+                u4 = im[i, 0]
+            else:
+                u4 = im[i, j + 1]
+
+            u1 = int(u1)
+            u2 = int(u2)
+            u3 = int(u3)
+            u4 = int(u4)
+
+            u = (D/(dx**2))*(u1 + u2 + u3 + u4 - 4 * int(im[i, j]))
+            im_var[i, j] = u
+
+    im = im + im_var
+    return im
+
+
+# ----diffusion using static boundary condition----
+def Difussion(im, D, dx):
+    im_var = im.copy()
+    for i in range(1, len(im) - 1):
+        for j in range(1, len(im.T) - 1):
+
+            u1 = im[i - 1, j]
+            u2 = im[i + 1, j]
+            u3 = im[i, j - 1]
+            u4 = im[i, j + 1]
+
+            u1 = int(u1)
+            u2 = int(u2)
+            u3 = int(u3)
+            u4 = int(u4)
+
+            u = (D / (dx ** 2)) * (u1 + u2 + u3 + u4 - 4 * int(im[i, j]))
+            im_var[i, j] = u
+
+    im = im + im_var
+    return im
+
+
+# ---diffusion using matrix method---
+def Difussion_f(im, D, dx):
+    im_var1 = im.copy()
+    im_var2 = im.copy()
+    
+    a1 = np.multiply((D/dx**2), generate_matrix(len(im.T)))
+    a2 = np.multiply((D/dx**2), generate_matrix(len(im)))
+    
+    for j in range(len(im)):
+        u1 = a1.dot(im[j, :])
+        im_var1[j, :] = u1
+    for i in range(len(im[0])):
+        u2 = a2.dot(im[:, i])
+        im_var2[:, i] = u2
+
+    im = im + im_var1 + im_var2
+    
+    return im
+
+
+def generate_matrix(n):
+    a = np.multiply(-2/1, np.identity(n))
+    ide = np.multiply(1/1, np.identity(n-1)).tolist()
+
+    for i in ide:
+        i.append(0)
+    
+    ide.insert(0,np.zeros(n).tolist())
+    ide = np.asarray(ide)
+    m = a + ide + ide.T
+
+    return m
